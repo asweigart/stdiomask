7c32726f8aa6e4af9da479541ca06a6658f1a25f
Al Sweigart <asweigart@gmail.com>
2019-11-22 19:54:00 -0800

    Adjusting setup.py to note that stdiomask can run on Python 2.7

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
